7c151e151f9c11347a7e526c444db967d8fae80d
ClaudioScordari <scordari22@gmail.com>
2024-03-06 16:02:31 +0100

    Aggiuna funzione show() protetta

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -7,16 +7,30 @@
         <div class="col">
             <div class="card">
                 <div class="card-body">
-                    <h1 class="my-3">
+                    <h1>
                         Progetti
                     </h1>
 
-                    @foreach ($projects as $project)
-                        <h2>Titolo progetto: {{ $project->name }}</h2>
-                        <br>
-                    @endforeach
+                    <br>
+
+                    <ul>
+                        @foreach ($projects as $project)
+                            <li class="mb-3">
+                                <h2>
+                                    Nome progetto: {{ $project->name }}
+                                </h2>
+
+                                <div class="pb-2 border-bottom border-3 border-dark d-inline-block">
+                                    <a class="btn btn-primary" href="{{ route('admin.projects.show', ['project' => $project->id]) }}">
+                                        Vedi il progetto
+                                    </a>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
 
                     <br>
+
                     La dashboard è una pagina privata (protetta dal middleware)
                 </div>
             </div>
---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('page-title', $project->name)
+
+@section('main-content')
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="my-2">
+                        {{ $project->name }}
+                    </h1>
+
+                    <br>
+
+                    La dashboard è una pagina privata (protetta dal middleware)
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
